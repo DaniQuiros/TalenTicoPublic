1f0542c59abfceee3c977e200624190e21c781be
carbalz <michaelcarballo73@gmail.com>
2023-08-16 02:25:54 -0600

    modificarEmpresa

---
+++ client/js/controllers/modificarEmpresa.controller.js
@@ -0,0 +1,14 @@
+const txtNombre = document.querySelector("#txtNombre")
+const txtCedula = document.querySelector("#txtIdentificacion")
+const txtAreaDescripcion = document.querySelector("#txtAreaDescripcion")
+const txtCorreo = document.querySelector("#txtCorreo")
+const txtPasswor = document.querySelector("#txtPasswor")
+const btnGuardar = document.querySelector("#btnGuardar")
+const btnCancelar = document.querySelector("#btnCancelar")
+
+const txtAreaMision = document.querySelector("#txtAreaMision")
+const txtAreaVision = document.querySelector("#txtAreaVision")
+const txtAreaBeneficios = document.querySelector("a")
+
+
+
