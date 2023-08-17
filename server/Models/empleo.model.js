4573774ecd81256dbf04f91bb58ca7447e9c1206
Daniela Quiros <dani.quiros.solis@gmail.com>
2023-08-17 12:51:48 -0600

    controaldores filtros

--- server/Models/empleo.model.js
+++ server/Models/empleo.model.js
@@ -2,14 +2,13 @@
 const mongoose = require('mongoose')
 
 // crear un esquema
-const schema_empleo = new mongoose.Schema({
-    
-    nombrePuesto:{type:String, require:true, unique:false},
-    rangoSalario:{type:Number, require:true, unique:false},
-    requisitos:{type:String, required:true, unique:true},
-    atributosCandidato:{type:String, required:true, unique:true},
-    descripcionPuesto:{type:String, required:true, unique:true},
-    estado:{type:String, default:'Activo'},
-})
+const SchemaEmpleo = new mongoose.Schema({
+  nombrePuesto: { type: String, required: true, unique: false },
+  rangoSalario: { type: Number, required: true, unique: false },
+  requisitos: { type: String, required: true, unique: true },
+  atributosCandidato: { type: String, required: true, unique: true },
+  descripcionPuesto: { type: String, required: true, unique: true },
+  estado: { type: String, default: "Activo" },
+});
 
 module.exports = mongoose.model('Empleo', schema_empleo, 'empleo')
